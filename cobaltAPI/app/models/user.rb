8cbc1163c140431fde535d495e437ccfd6a4c5b7
Edward Heaver <edward.heaver01@gmail.com>
2020-07-07 13:56:08 +0100

    added faker and jwt gem and user validations

--- cobaltAPI/app/models/user.rb
+++ cobaltAPI/app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
     has_secure_password
-    validates_presence_of :first_name, :last_name, :username
+    validates_presence_of :first_name, :last_name
+    validates :username, uniqueness: true
+    validates :email_address, uniqueness: true
 
 end
