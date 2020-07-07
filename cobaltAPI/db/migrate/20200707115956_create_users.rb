3da7cd4a72543fbc1d2a7666c0d95a71c3634882
Edward Heaver <edward.heaver01@gmail.com>
2020-07-07 13:07:20 +0100

    wrote users migration

--- cobaltAPI/db/migrate/20200707115956_create_users.rb
+++ cobaltAPI/db/migrate/20200707115956_create_users.rb
@@ -1,7 +1,15 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-
+      t.has_many :bookings
+      t.has_many :role
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :password_digest
+      t.string :mobile_number
+      t.string :email_address
+      
       t.timestamps
     end
   end
