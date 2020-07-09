ac2d1d23331e9dc542c86d02b37d82cad14e12d9
Edward Heaver <edward.heaver01@gmail.com>
2020-07-09 17:29:04 +0100

    added bootstrap CDN and created app store

--- cobalt-tbl-manager/src/index.js
+++ cobalt-tbl-manager/src/index.js
@@ -1,12 +1,26 @@
+// Normal react imports
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+
+// Boostrap import so components work inside children - stylesheet is also CDN in html
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import middleware for async and store management
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import rootReducer from './reducers/rootReducer';
+import thunk from 'redux-thunk';
+
+// Create store
+const store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store = {store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
