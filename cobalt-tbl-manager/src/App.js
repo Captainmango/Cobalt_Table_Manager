57dc24e8801c7f64a8b7a3ca450c16f5af0c6980
Edward Heaver <edward.heaver01@gmail.com>
2020-07-09 18:22:33 +0100

    implemented react router and have test routes working

--- cobalt-tbl-manager/src/App.js
+++ cobalt-tbl-manager/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 
 
 export class App extends React.Component {
   render() {
     return (
-      <div>
-        <h1>This is a test</h1>
-      </div>
+      <Router>
+        <div>
+          <Route exact path="/" render={() => <h1><b>This is a test for client side routing</b></h1> }/>
+          <Route exact path="/bookings" render={()=> <h2><b>This is a test for the bookings route</b></h2>} />
+        </div>
+      </Router>
     )
   }
 }
