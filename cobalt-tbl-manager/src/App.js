5d1f590f9ec6025e3affc0c2812b8a98779a34a9
Edward Heaver <edward.heaver01@gmail.com>
2020-07-10 13:28:18 +0100

    added all routes to app.js

--- cobalt-tbl-manager/src/App.js
+++ cobalt-tbl-manager/src/App.js
@@ -8,8 +8,12 @@ export class App extends React.Component {
     return (
       <Router>
         <div>
-          <Route exact path="/" render={() => <h1><b>This is a test for client side routing</b></h1> }/>
+          <Route exact path="/" render={() => <h1 className="text-center"><b>This is a test for client side routing</b></h1> }/>
           <Route exact path="/bookings" render={()=> <h2><b>This is a test for the bookings route</b></h2>} />
+          <Route exact path="/locations/:locationId/bookings" render={() => <h1>This is the locations booking route</h1>} />
+          <Route exact path="/bookings/:bookingId" render={() => <h1>This will be the bookings show page</h1>} />
+          <Route exact path="/locations" render={() => <h1>This will show all the locations to book at or the owner's locations</h1>} />
+          <Route exact path="/locations/:locationId" render={() => <h1> This will be the locations show page</h1>}/>
         </div>
       </Router>
     )
