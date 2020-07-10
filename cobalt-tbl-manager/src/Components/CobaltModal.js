665e86240e045cd80f91d64fbe0f17d4183eecd1
Edward Heaver <edward.heaver01@gmail.com>
2020-07-10 13:10:10 +0100

    editting CSS and created components

---
+++ cobalt-tbl-manager/src/Components/CobaltModal.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Modal from 'react-bootstrap/Modal'
+
+export default function CobaltModal() {
+    return (
+        <>
+            <Modal>
+                
+            </Modal>
+        </>
+    )
+}
