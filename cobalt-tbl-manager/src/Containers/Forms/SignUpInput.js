6e91d218d47154aa23f71550da64ccbd4b1777a4
Edward Heaver <edward.heaver01@gmail.com>
2020-07-10 16:46:07 +0100

    working on sign up form

--- cobalt-tbl-manager/src/Containers/Forms/SignUpInput.js
+++ cobalt-tbl-manager/src/Containers/Forms/SignUpInput.js
@@ -1,7 +1,37 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 
-export class SignUpInput extends Component {
+class SignUpInput extends Component {
+
+    state = {
+        username: "",
+        password: "",
+        first_name: "",
+        last_name: "",
+        mobile_number: "",
+        email_address: ""
+    }
+
+    handleOnChange(event) {
+        this.setState({
+          name: event.target.value,
+        });
+      }
+
+    handleOnSubmit(event) {
+        event.preventDefault();
+        this.props.addUser(this.state);
+        this.props.createUser(this.state);
+        this.setState({
+            username: "",
+            password: "",
+            first_name: "",
+            last_name: "",
+            mobile_number: "",
+            email_address: ""
+        });
+    }
+
     render() {
         return (
             <div>
