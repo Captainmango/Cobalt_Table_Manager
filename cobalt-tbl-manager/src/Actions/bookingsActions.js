ecf3745283c8943658c62eca33eb8627154eef48
Edward Heaver <edward.heaver01@gmail.com>
2020-07-22 20:39:40 +0100

    writing bookings actions and reducers

---
+++ cobalt-tbl-manager/src/Actions/bookingsActions.js
@@ -0,0 +1,34 @@
+export const fetchBookings = (user_id) => {
+    return (dispatch) => {
+        dispatch({type: "FETCH_BOOKINGS"});
+        fetch(`http://localhost:3001/users/${user_id}/bookings`, {
+            method: 'GET',
+            mode: 'cors',
+            headers: {'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.token}` }
+        })
+        .then(response => response.json())
+        .then(returnData => {
+            let bookings = returnData.data;
+            dispatch({type: "ADD_BOOKINGS", bookings: bookings})
+        })
+
+    }
+}
+
+export const postBooking = (user_id, booking) => {
+    return (dispatch) => {
+        dispatch({type: "POST_BOOKING"});
+        fetch(`http://localhost:3001/users/${user_id}/bookings`, {
+            method: 'POST',
+            mode: 'cors',
+            headers: {'Content-Type': 'application/json',
+                        'Authorization': `Bearer ${localStorage.token}` },
+            body: JSON.stringify(booking)
+        })
+        .then(response => response.json())
+        .then(returnData => {
+            if(returnData)
+        })
+    }
+}
--- cobalt-tbl-manager/src/Reducers/bookingsReducer.js
+++ cobalt-tbl-manager/src/Reducers/bookingsReducer.js
@@ -1,10 +1,30 @@
-function bookingsReducer(bookings = [], action){
+function bookingsReducer(state = {bookings: [], requesting: false }, action){
     switch (action.type){
 
+        case "ADD_BOOKINGS":
+            return {...state,
+                bookings: [action.bookings],
+                requesting: false
+            }
+
+        case "FETCH_BOOKINGS":
+            return {
+                ...state,
+                bookings: [...state.bookings],
+                requesting: true
+            }
+
+        case "POST_BOOKING":
+            return {
+                ...state,
+                booking: [action.booking],
+                requesting: true
+            }
+
         default:
-            return bookings;
+            return state;
 
     }
 }
 
-export default bookingsReducer;
+export default bookingsReducer
