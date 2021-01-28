10cfd1d0b83478f6f273f4560521bd8633043219
Edward <edward.heaver01@gmail.com>
2021-01-28 14:54:22 +0000

    complete add flash message action

--- cobalt-tbl-manager/src/Components/Flash.js
+++ cobalt-tbl-manager/src/Components/Flash.js
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from 'react';
 import { Alert } from 'react-bootstrap';
 import Bus from '../utils/Bus';
 
-const Flash = () => {
+const Flash = (title, message, alertType) => {
   
     let [visibility, setVisibility] = useState(false);
 //use FlashMessage package to wrap the alert and use same syntax. Maybe break visibility state into Redux?
 
     return (
-        visibility && <Alert style={{position: 'fixed', width: '100%'}} variant={type} onClose={ () => { setVisibility(false) } } dismissible>
+        visibility && <Alert style={{position: 'fixed', width: '100%'}} variant={alertType} onClose={ () => { setVisibility(false) } } dismissible>
             <Alert.title>{title}</Alert.title>
             <p>{message}</p>
         </Alert>
--- cobalt-tbl-manager/src/Reducers/flashMessagesReducer.js
+++ cobalt-tbl-manager/src/Reducers/flashMessagesReducer.js
@@ -1,6 +1,18 @@
+import shortid from 'shortid';
+
 function flashMessagesReducer(state=[], action = {}) {
     switch (action.type){
-        
+        case ADD_FLASH_MESSAGE:
+            return [
+                ...state,
+                {   
+                    id: shortid.generate(),
+                    title: action.flash.title,
+                    message: action.flash.message,
+                    alertType: action.flash.alertType
+                }
+            ]
+        default: return state
     }
 
 }
