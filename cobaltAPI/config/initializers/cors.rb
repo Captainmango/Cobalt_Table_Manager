d49b9b5ed087066cc6b68229c93b07601052e8d6
Edward <edward.heaver01@gmail.com>
2021-02-02 17:53:04 +0000

    added comment in cors file

--- cobaltAPI/config/initializers/cors.rb
+++ cobaltAPI/config/initializers/cors.rb
@@ -9,6 +9,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'localhost:3000'
 
+    # ensure we change this when deploying the app to the correct host name
+
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
