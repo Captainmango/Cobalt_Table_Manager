41e63ddd2c9f48ce5874a7acee9b1681eaa3251d
Edward <edward.heaver01@gmail.com>
2021-02-03 12:36:35 +0000

    created user migration

--- cobaltAPI/app/models/user.rb
+++ cobaltAPI/app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
     has_many :bookings
     has_many :bookings, through: :restaurants
+
+    validates_presence_of :email
+    validates :email, uniqueness: true
+
+    has_secure_password
+
 end
--- cobaltAPI/db/migrate/20210203122023_create_users.rb
+++ cobaltAPI/db/migrate/20210203122023_create_users.rb
@@ -1,6 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :username
+      t.string :email_addresss
+      t.string :mobile_number
 
       t.timestamps
     end
