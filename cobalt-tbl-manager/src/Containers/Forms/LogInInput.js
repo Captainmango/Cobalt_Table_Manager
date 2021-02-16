c4aee6368cc83d797d83c6a14fe1d8a7b3231547
Edward <edward.heaver01@gmail.com>
2021-02-16 16:59:48 +0000

    styled log in form

--- cobalt-tbl-manager/src/Containers/Forms/LogInInput.js
+++ cobalt-tbl-manager/src/Containers/Forms/LogInInput.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react'
 import { connect } from 'react-redux'
 import Form from 'react-bootstrap/Form';
-import InputGroup from 'react-bootstrap/InputGroup';
+import { useHistory } from 'react-router-dom'
 import FormControl from 'react-bootstrap/FormControl';
 import { logInUser } from '../../Actions/userActions';
 import Button from 'react-bootstrap/Button';
 import { toast } from 'react-toastify';
+import { Container, Row, Col } from 'react-bootstrap';
 
 
 
 const LogInInput = (props) => {
+
+    let history = useHistory();
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
@@ -27,13 +30,20 @@ const LogInInput = (props) => {
         setPassword("");
     }
 
+    const handleOnCancel = (event) => {
+        event.preventDefault();
+        setUsername("");
+        setPassword("");
+        history.push("/");
+    }
+
         return (
-            <>
+            <Container className="pt-2">
+                <h2 className="text-center pb-3">Log in</h2>
+                <Row className="justify-content-center">
+                    <Col lg={{span: 5}}>
                 <Form>
-                    <InputGroup>
-                    <InputGroup.Prepend>
-                        <InputGroup.Text id="basic-addon1">@</InputGroup.Text>
-                    </InputGroup.Prepend>
+                    <Form.Row className="p-2">
                     <FormControl
                         value = {username}
                         onChange={ (event) => setUsername(event.target.value) }
@@ -42,7 +52,9 @@ const LogInInput = (props) => {
                         aria-describedby="basic-addon1"
                         name="username"
                     />
+                    </Form.Row>
                     
+                    <Form.Row className="p-2">
                     <FormControl
                         value = {password}
                         onChange={ (event) => setPassword(event.target.value) }
@@ -51,10 +63,15 @@ const LogInInput = (props) => {
                         type="password"
                         name="password"
                     />
-                    <Button onClick={ (event) => { handleOnSubmit(event); } } variant="btn btn-outline-secondary">Log in</Button>
-                    </InputGroup>
+                    </Form.Row>
+                    <Form.Row className="justify-content-center">
+                        <Button className="m-2" variant="success" onClick={ (event) => { handleOnSubmit(event); } }>Log in</Button>
+                        <Button className="m-2" variant="danger" onClick={ (event) => { handleOnCancel(event); } }>Cancel</Button>
+                    </Form.Row>
                     </Form>
-            </>
+                    </Col>
+                    </Row>
+            </Container>
         )
     }
 
